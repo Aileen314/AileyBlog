9ed0bf61e41f612a4d67aef55f2e0a0451b3cb08
杨艾林 <18728116234>
2022-11-28 17:55:12 +0800

    服务注册

--- live-ailey-life/src/main/java/live/ailey/AileyLifeApplication.java
+++ live-ailey-life/src/main/java/live/ailey/AileyLifeApplication.java
@@ -2,8 +2,10 @@ package live.ailey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 让注册中心发现并扫描该服务
 public class AileyLifeApplication {
 
     public static void main(String[] args) {
