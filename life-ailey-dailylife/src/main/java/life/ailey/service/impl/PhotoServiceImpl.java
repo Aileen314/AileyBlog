30c2a1cf7dd2db44fa7455f40569e306a32be8d0
杨艾林 <18728116234>
2022-11-30 19:03:57 +0800

    项目成功跑通（请求->数据库）

--- life-ailey-dailylife/src/main/java/life/ailey/service/impl/PhotoServiceImpl.java
+++ life-ailey-dailylife/src/main/java/life/ailey/service/impl/PhotoServiceImpl.java
@@ -5,6 +5,7 @@ import life.ailey.mapper.PhotoMapper;
 import life.ailey.service.IPhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class PhotoServiceImpl implements IPhotoService {
     @Autowired
     private PhotoMapper photoMapper;
 
+    @Override
+    @Transactional
     public List<Photo> getList() {
         List<Photo> list = photoMapper.getList();
         return list;
